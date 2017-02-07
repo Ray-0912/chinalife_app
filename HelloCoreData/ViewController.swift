9fd859ef76f8105fb88456a85e976b8ad5e89ff0
楊昇融 <yangshengrong@yangshengrongs-mac.local>
2017-02-07 19:23:35 +0800

    Initial Commit

---
+++ HelloCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloCoreData
+//
+//  Created by 楊昇融 on 2017/2/7.
+//  Copyright © 2017年 楊昇融. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
